64e989d7265aa0cc6db197028661153542866466
okld <65978885+okld@users.noreply.github.com>
2022-03-11 01:28:27 +0100

    Fix constant name

--- streamlit_elements/core/render.py
+++ streamlit_elements/core/render.py
@@ -3,8 +3,8 @@ from streamlit.components.v1 import declare_component
 from streamlit_elements import version
 
 if version.__release__:
-    ELEMENT_FRONTEND = {"path": (Path(version.__file__).parent/"frontend/build").resolve()}
+    ELEMENTS_FRONTEND = {"path": (Path(version.__file__).parent/"frontend/build").resolve()}
 else:
-    ELEMENT_FRONTEND = {"url": "http://localhost:3001"}
+    ELEMENTS_FRONTEND = {"url": "http://localhost:3001"}
 
-render_component = declare_component("streamlit_elements", **ELEMENT_FRONTEND)
+render_component = declare_component("streamlit_elements", **ELEMENTS_FRONTEND)
