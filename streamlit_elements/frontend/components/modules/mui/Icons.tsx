a6890471b7778c1eaee351050fe874d4ee93ef9f
okld <65978885+okld@users.noreply.github.com>
2022-03-14 00:18:25 +0100

    Cache loaded icons to avoid disappearing and reappearing on reload

--- streamlit_elements/frontend/components/modules/mui/Icons.tsx
+++ streamlit_elements/frontend/components/modules/mui/Icons.tsx
@@ -1,10 +1,21 @@
 import dynamic from "next/dynamic"
 import ElementsLoading from "../../ElementsLoading"
 
-const loadMuiIcons: ElementsLoader = element =>
-    dynamic(
+const cachedIcons: ElementsRecord = {}
+
+const loadMuiIcons: ElementsLoader = element => {
+    if (cachedIcons.hasOwnProperty(element)) {
+        return cachedIcons[element]
+    }
+
+    const icon = dynamic(
         () => import("@mui/icons-material").then((module: any) => module[element]),
         { loading: ElementsLoading, ssr: false }
     )
+
+    cachedIcons[element] = icon
+
+    return icon
+}
 
 export default loadMuiIcons
