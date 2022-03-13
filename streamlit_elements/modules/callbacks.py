297ec91e5123cf3d9058e85b82a55298320eae03
okld <65978885+okld@users.noreply.github.com>
2022-03-14 00:20:18 +0100

    Remove lazy parameter from sync, add a new lazy function
    - Sync now won't take any callback, it is only used to sync session state with event values
    - Lazy can be used on ElementsCallback or callable

--- streamlit_elements/modules/callbacks.py
+++ streamlit_elements/modules/callbacks.py
@@ -1,10 +1,21 @@
-import re
-
+from typing import Callable, List, Union
 from streamlit import session_state
 from streamlit_elements.core.callback import ElementsCallback
 
-__all__ = ["sync"]
+__all__ = ["sync", "lazy"]
 
 
-def sync(*session_state_keys, lazy=False):
-    return ElementsCallback(session_state.update, args=session_state_keys, lazy=lazy)
+def sync(*session_state_keys: List[str]):
+    # Replace None keys with an underscore.
+    session_state_keys = (key if key is not None else "_" for key in session_state_keys)
+
+    return ElementsCallback(session_state.update, params=session_state_keys)
+
+
+def lazy(callback: Union[Callable, ElementsCallback]):
+    if isinstance(callback, Callable):
+        callback = ElementsCallback(callback, params=None)
+
+    callback.lazy()
+
+    return callback
