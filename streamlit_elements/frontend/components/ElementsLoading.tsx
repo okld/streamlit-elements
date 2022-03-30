3874a407a5284651fc3da2c1629e7fc42b0b0a57
okld <65978885+okld@users.noreply.github.com>
2022-03-30 21:38:49 +0200

    Update ElementsLoading skeleton height

--- streamlit_elements/frontend/components/ElementsLoading.tsx
+++ streamlit_elements/frontend/components/ElementsLoading.tsx
@@ -2,7 +2,7 @@ import { Skeleton, Box } from "@mui/material"
 
 const ElementsLoading = () => (
   <Box sx={{ flex: 1, padding: "0 5px 0 5px" }}>
-    <Skeleton height={40} width="100%" animation="wave" />
+    <Skeleton height={35} width="100%" animation="wave" />
   </Box>
 )
 
